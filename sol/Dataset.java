da8bb3438c945c0d21178b225b3386eac944e52b
Margaret Li <MLi0411>
2025-01-19 11:52:19 -0500

    changed selectiontype switch case to if else

--- sol/Dataset.java
+++ sol/Dataset.java
@@ -7,20 +7,27 @@ import src.AttributeSelection;
 import src.IDataset;
 import src.Row;
 
+import java.util.*;
+
 /**
  * A class representing a training dataset for the decision tree
  */
 // TODO: Uncomment this once you've implemented the methods in the IDataset interface!
 public class Dataset /* implements IDataset */ {
+    private List<String> attributeList;
+    private List<Row> dataObjects;
+    private AttributeSelection selectionType;
 
     /**
      * Constructor for a Dataset object
      * @param attributeList - a list of attributes
      * @param dataObjects -  a list of rows
-     * @param attributeSelection - an enum for which way to select attributes
+     * @param selectionType - an enum for which way to select attributes
      */
-    public Dataset(List<String> attributeList, List<Row> dataObjects, AttributeSelection attributeSelection) {
-        // TODO: implement the constructor! (Hint: take a look at `getAttributeToSplitOn`)
+    public Dataset(List<String> attributeList, List<Row> dataObjects, AttributeSelection selectionType) {
+        this.attributeList = new ArrayList<String>(attributeList);
+        this.dataObjects = new ArrayList<Row>(dataObjects);
+        this.selectionType = selectionType;
     }
 
 //    TODO: Uncomment this once you've completed the constructor!
@@ -38,4 +45,22 @@ public class Dataset /* implements IDataset */ {
 //        }
 //        throw new RuntimeException("Non-Exhaustive Switch Case");
 //    }
+//
+
+//    TODO: Uncomment this once you've completed the constructor!
+    public String getAttributeToSplitOn() {
+        if (this.selectionType == AttributeSelection.ASCENDING_ALPHABETICAL) {
+            return this.attributeList.stream().sorted().toList().get(0);
+        }
+        if (this.selectionType == AttributeSelection.DESCENDING_ALPHABETICAL) {
+            return this.attributeList.stream().sorted().toList().get(this.attributeList.size() - 1);
+        }
+        if (this.selectionType == AttributeSelection.RANDOM) {
+            Random r = new Random();
+            int randomIndex = r.nextInt(this.attributeList.size());
+            return this.attributeList.get(randomIndex);
+        }
+
+        throw new RuntimeException("Non-Exhaustive Switch Case");
+    }
 }
