d992ff05378e1d923780d4b0a9eacd699f70ad57
Margaret Li <MLi0411>
2025-01-19 16:17:13 -0500

    updated selectiontype switch cases to if else

--- sol/Dataset.java
+++ sol/Dataset.java
@@ -7,17 +7,11 @@ import src.AttributeSelection;
 import src.IDataset;
 import src.Row;
 
-import java.util.*;
-
 /**
  * A class representing a training dataset for the decision tree
  */
 // TODO: Uncomment this once you've implemented the methods in the IDataset interface!
 public class Dataset /* implements IDataset */ {
-    private List<String> attributeList;
-    private List<Row> dataObjects;
-    private AttributeSelection selectionType;
-
     /**
      * Constructor for a Dataset object
      * @param attributeList - a list of attributes
@@ -25,42 +19,21 @@ public class Dataset /* implements IDataset */ {
      * @param selectionType - an enum for which way to select attributes
      */
     public Dataset(List<String> attributeList, List<Row> dataObjects, AttributeSelection selectionType) {
-        this.attributeList = new ArrayList<String>(attributeList);
-        this.dataObjects = new ArrayList<Row>(dataObjects);
-        this.selectionType = selectionType;
+        // TODO: implement the constructor! (Hint: take a look at `getAttributeToSplitOn`)
     }
 
 //    TODO: Uncomment this once you've completed the constructor!
 //    public String getAttributeToSplitOn() {
-//        switch (this.selectionType) {
-//            case AttributeSelection.ASCENDING_ALPHABETICAL -> {
-//                return this.attributeList.stream().sorted().toList().get(0);
-//            }
-//            case AttributeSelection.DESCENDING_ALPHABETICAL -> {
-//                return this.attributeList.stream().sorted().toList().get(this.attributeList.size() - 1);
-//            }
-//            case AttributeSelection.RANDOM -> {
-//                // TODO: Implement random attribute selection!
-//            }
+//        if (this.selectionType == AttributeSelection.ASCENDING_ALPHABETICAL) {
+//            return this.attributeList.stream().sorted().toList().get(0);
 //        }
-//        throw new RuntimeException("Non-Exhaustive Switch Case");
-//    }
+//        if (this.selectionType == AttributeSelection.DESCENDING_ALPHABETICAL) {
+//            return this.attributeList.stream().sorted().toList().get(this.attributeList.size() - 1);
+//        }
+//        if (this.selectionType == AttributeSelection.RANDOM) {
+//            // TODO: Implement random attribute selection!
+//        }
 //
-
-//    TODO: Uncomment this once you've completed the constructor!
-    public String getAttributeToSplitOn() {
-        if (this.selectionType == AttributeSelection.ASCENDING_ALPHABETICAL) {
-            return this.attributeList.stream().sorted().toList().get(0);
-        }
-        if (this.selectionType == AttributeSelection.DESCENDING_ALPHABETICAL) {
-            return this.attributeList.stream().sorted().toList().get(this.attributeList.size() - 1);
-        }
-        if (this.selectionType == AttributeSelection.RANDOM) {
-            Random r = new Random();
-            int randomIndex = r.nextInt(this.attributeList.size());
-            return this.attributeList.get(randomIndex);
-        }
-
-        throw new RuntimeException("Non-Exhaustive Switch Case");
-    }
+//        throw new RuntimeException("Non-Exhaustive Selection Type");
+//    }
 }
